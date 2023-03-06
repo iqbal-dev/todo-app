289edc148edb284707c481297d8446e00e649ec0
iqbal-devgit config --global user.name Your <pkmdiqbal@gmail.com>
2023-03-06 23:36:53 +0600

    todo reducer

---
+++ src/redux/todos/initialStat.js
@@ -0,0 +1,15 @@
+const initialState = [
+  {
+    id: 1,
+    text: "React redux",
+    completed: true,
+  },
+  {
+    id: 1,
+    text: "React redux",
+    completed: true,
+    color: "red",
+  },
+];
+
+export default initialState;
---
+++ src/redux/todos/reducer.js
@@ -0,0 +1,57 @@
+import {
+  ADDED,
+  ALLCOMPLETED,
+  CLEARCOMPLETED,
+  COLORSELECETED,
+  DELETED,
+  TOGGLED,
+} from "./actionTypes";
+import initialState from "./initialStat";
+
+const nextId = (todos) => {
+  let id = todos.reduce((acc, curr) => Math.max(curr.id, acc.id), -1);
+  return id++;
+};
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADDED:
+      return [
+        ...state,
+        {
+          text: action.payload,
+          id: nextId(state),
+        },
+      ];
+    case DELETED:
+      return state.filter((todo) => todo.id !== action.payload);
+
+    case TOGGLED:
+      return state.map((todo) => {
+        if (todo.id !== action.payload) {
+          return todo;
+        }
+        return {
+          ...todo,
+          completed: !todo.completed,
+        };
+      });
+    case COLORSELECETED:
+      return state.map((todo) => {
+        if (todo.id !== action.payload.id) {
+          return todo;
+        }
+        return {
+          ...todo,
+          color: action.payload.color,
+        };
+      });
+    case ALLCOMPLETED:
+      return state.map((todo) => ({ ...todo, completed: true }));
+    case CLEARCOMPLETED:
+      return [];
+    default:
+      return state;
+  }
+};
+
+export default reducer;
