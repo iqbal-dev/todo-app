fd5a8cf7f833bdf3081235d924a2182c99ad3343
iqbal-devgit config --global user.name Your <pkmdiqbal@gmail.com>
2023-03-06 21:57:01 +0600

    redux all file created and action types added

---
+++ src/redux/todos/actionTypes.js
@@ -0,0 +1,6 @@
+export const ADDED = "todos/added";
+export const DELETED = "todos/deleted";
+export const TOGGLED = "todos/toggled";
+export const COLORSELECETED = "todos/colorSelected";
+export const ALLCOMPLETED = "todos/allCompleted";
+export const CLEARCOMPLETED = "todos/clearcompleted";
